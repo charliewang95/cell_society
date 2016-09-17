9916cbc1058246943e1d96d4ba3d0a4b1066a063
Charlie Wang <qw42@duke.edu>
2016-09-17 17:10:01 -0400

    finish design.md

--- src/layout/Cell.java
+++ src/layout/Cell.java
@@ -1,11 +1,39 @@
 package layout;
 
+import java.util.ArrayList;
+
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Cell {
-	Rectangle myRec;
+	private Rectangle myRec;
+	private int myState;
+	private ArrayList<Cell> myNeighbor;
 	
-	public Cell (int x, int y, int length, int width) {
+	public Cell (int x, int y, int length, int width, int state, Color c) {
 		myRec = new Rectangle (x, y, length, width);
+		init(state, c);
+	}
+	
+	private void init(int state, Color c) {
+		myState = state;
+		myRec.setFill(c);
+		myNeighbor = new ArrayList<Cell>();
+	}
+	
+	public void addNeighbor(Cell cell) {
+		myNeighbor.add(cell);
+	}
+	
+	public void setState(int newState) {
+		myState = newState;
+	}	
+	
+	public int getState() {
+		return myState;
+	}
+	
+	public Rectangle getRec() {
+		return myRec;
 	}
 }

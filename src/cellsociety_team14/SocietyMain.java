d80eea729858a60c0d24c14f88583021dea7623e
Charlie Wang <qw42@duke.edu>
2016-09-17 15:51:39 -0400

    added some classes

---
+++ src/cellsociety_team14/SocietyMain.java
@@ -0,0 +1,10 @@
+package cellsociety_team14;
+
+public class SocietyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/layout/rule/FireRule.java
@@ -0,0 +1,5 @@
+package layout.rule;
+
+public class FireRule {
+
+}

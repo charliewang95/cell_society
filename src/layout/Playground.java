d811739ad2ae150b374e22a4c64cde7f1aeb1da5
Joy Kim <jmk77@duke.edu>
2016-09-22 22:40:02 -0400

    add xml parse to playground

--- src/layout/Playground.java
+++ src/layout/Playground.java
@@ -1,6 +1,7 @@
 package layout;
 
 
+import java.io.File;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.Group;
@@ -8,12 +9,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import layout.rule.FireRule;
+import xml.XMLParser;
+import xml.factory.FireRuleXMLFactory;
+import xml.factory.XMLFactoryException;
 
 public class Playground {
 	
 	public static final int FRAMES_PER_SECOND = 60;
 	private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
 	private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+	private static final String XML_FILES_LOCATION = "data/xml/";
+    private static final String XML_SUFFIX = ".xml";
+    private String myFileName;
 	
 	Group root;
 	Rule sim;
@@ -22,12 +29,17 @@ public class Playground {
 	int SIZEX = 0;
 	int SIZEY = 0;
 	
-	public void init(Stage s) {
+	public void init(Stage s, String file) {
 		
 		//READ XML FILE
 		//get file and author name, global config parameters, dimensions of grid and initial states of the cell
 		//http://stackoverflow.com/questions/428073/what-is-the-best-simplest-way-to-read-in-an-xml-file-in-java-application
 		//http://stackoverflow.com/questions/7704827/java-reading-xml-file
+		
+		getParsedObject(myFileName);
+		
+		
+		
 		
 		
 		//s.setTitle("sksks")
@@ -36,7 +48,6 @@ public class Playground {
 		
 		//how to consider user input
 		
-		sim = new FireRule(LENGTH, WIDTH, SIZEX, SIZEY);
 		
 		//determine how to take XML instructions for initial states into each square: Rule.initState()
 		
@@ -51,6 +62,36 @@ public class Playground {
 		animation.play();
 		
 		
+	}
+
+	public String getFileName() {
+		return myFileName;
+	}
+	
+	public void setFileName(String file) {
+		myFileName = file;
+	}
+
+
+	private void getParsedObject(String fileName) {
+		XMLParser parser = new XMLParser();
+		FireRuleXMLFactory factory = new FireRuleXMLFactory();
+        File f = new File(XML_FILES_LOCATION+fileName);
+        Rule ruleInXML;
+        
+        if (f.isFile() && f.getName().endsWith(XML_SUFFIX)) {
+            try {
+                ruleInXML = factory.getRule(parser.getRootElement(f.getAbsolutePath()));
+                sim = ruleInXML;
+            }
+            catch (XMLFactoryException e) {
+                System.err.println("Reading file " + f.getPath());
+                e.printStackTrace();
+                //REDO EXCEPTION so that it'll just give pop up window, enter a valid file. 
+            }
+        }
+        
+        
 	}
 	
 	
